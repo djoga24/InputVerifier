2512c28fc8b73e4880717bc7c96ae5c8aeb27d8d
Djoga24 <djoga1@ocdsb.ca>
2023-04-05 22:45:30 -0400

    Program to check if the user has entered a valid integer. Includes error handling

--- InputVerifier.java
+++ InputVerifier.java
@@ -1,50 +1,66 @@
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.*;
 
-public class Testing {
-    public static void main(String[] args) {
-        // Create components
-        JFrame frame = new JFrame("Integer Input Verifier");
-        JLabel inputLabel = new JLabel("Enter an integer:");
-        JTextField inputField = new JTextField(10);
-        JButton verifyButton = new JButton("Verify");
-        JButton cancelButton = new JButton("Cancel");
-        JLabel resultLabel = new JLabel("");
+public class InputVerifier {
+    // Declare instance variables for components used in the GUI
+
+    private JFrame main_frame;
+    private JLabel inputLabel;
+    private JTextField inputField;
+    private JButton verifyButton;
+    private JButton quitButton;
+    private JLabel resultLabel;
+
+    public InputVerifier() {
+        main_frame = new JFrame("Integer Input Verifier");
+
+        // Create components needed in GUI
+        inputLabel = new JLabel("Enter an integer:");
+        inputField = new JTextField(9);
+        verifyButton = new JButton("Verify");
+        quitButton = new JButton("Quit");
+        resultLabel = new JLabel("");
 
         // Add components to the frame
         JPanel panel = new JPanel();
         panel.add(inputLabel);
         panel.add(inputField);
         panel.add(verifyButton);
-        panel.add(cancelButton);
+        panel.add(quitButton);
         panel.add(resultLabel);
-        frame.add(panel);
+        main_frame.add(panel);
 
         // Set frame properties
-        frame.setSize(300, 120);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setVisible(true);
+        main_frame.setSize(320, 120);
+        main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_frame.setLocationRelativeTo(null);
+        main_frame.setResizable(false);
+        main_frame.setVisible(true);
 
-        // Add action listeners
-        verifyButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String input = inputField.getText();
+        // Add action listeners to buttons. Lambda functionns so that when the button is pressed, it will call the function.
+        verifyButton.addActionListener(e -> checkInteger());
+        // Disposes of frame when button clicked
+        quitButton.addActionListener(e -> main_frame.dispose());
+    }
 
-                try {
-                    int value = Integer.parseInt(input);
-                    resultLabel.setText(input + " is an integer.");
-                } catch (NumberFormatException ex) {
-                    resultLabel.setText(input + " is not an integer.");
-                }
-            }
-        });
+    // Method that checks if the input is an integer when the verify button is clicked
+    public void checkInteger() {
+        String input = inputField.getText();
+        try {
+            // Parse input to integer
+            int value = Integer.parseInt(input);
+            // Display message if input is an integer
+            resultLabel.setText(input + " is an integer.");
+            inputField.setText("");
+        } catch (NumberFormatException ex) {
+            // Display message if input is not an integer
+            JOptionPane.showMessageDialog(main_frame, input + " is not an integer");
+            inputField.setText("");
+        }
+    }
 
-        cancelButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-            }
-        });
+    // Main method that runs the program
+    public static void main(String[] args) {
+        new InputVerifier();
     }
 }
